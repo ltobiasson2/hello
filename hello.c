91148e4e9942e3e0e11aba59904a2efbc2f090fd
Lilli Tobiasson <ltobiasson2@huskers.unl.edu>
2021-08-26 17:58:08 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Author: Lilli Tobiasson
+ *          ltobiasson2@unl.edu
+ * Date 2021/08/26
+ * 
+ * a simple hello program in c
+ * 
+ */ 
+ 
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+ 
+   printf("Lilli Tobiasson\n");
+   
+   return 0;
+ }
